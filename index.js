e08b38f0b8e9134092e1f4d1033649e8fa7b1932
DanielMunoz4190 <A01641792@itesm.mx>
2024-03-13 15:13:45 -0600

    final version

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import config from "./config.js";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
-app.get('/', (req, res) => {
-  res.send('Hola Mundo');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/generate', async (req, res) => {
+  const { tipo, descripcion } = req.body; // Extrae tipo y descripción del cuerpo de la solicitud
+
+  let promptText;
+  switch (tipo) {
+    case 'cuento':
+      promptText = `Escribe un cuento corto sobre ${descripcion}`;
+      break;
+    case 'poema':
+      promptText = `Escribe un poema corto sobre ${descripcion}`;
+      break;
+    case 'producto':
+      promptText = `Genera la descripción de un producto sobre ${descripcion}`;
+      break;
+    default:
+      return res.status(400).send('Tipo no válido.');
+  }
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["human", promptText],
+  ]);
+
+  const model = new ChatOpenAI({
+    apiKey: config.apiKey,
+  });
+  const outputParser = new StringOutputParser();
+  
+  const chain = prompt.pipe(model).pipe(outputParser);
+  
+  try {
+    const response = await chain.invoke({});
+    res.json({ response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al generar la respuesta.');
+  }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
